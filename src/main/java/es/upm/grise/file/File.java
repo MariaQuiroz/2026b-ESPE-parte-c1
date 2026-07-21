9c41a99749af8cbc5ccbc2a5472f7cb173907053
Maria Quiroz <mlquiroz1@espe.edu.ec>
2026-07-21 07:38:20 -0500

    Implementación del paso 5 y las pruebas unitarias

--- src/main/java/es/upm/grise/file/File.java
+++ src/main/java/es/upm/grise/file/File.java
@@ -16,32 +16,72 @@ public class File {
      * Method to code/test
      */
     public File(FileType type, Location location) {
-    	
+        this.type = type;
+        this.location = location;
+        this.content = new ArrayList<>();
     }
 
     /*
      * Method to code/test
      */
-    public void addProperty(char[] content) throws InvalidContentException, WrongFileTypeException {
-    	
+    public void addProperty(char[] content) 
+        throws InvalidContentException, WrongFileTypeException {
 
+    if (content == null) {
+        throw new InvalidContentException();
     }
+
+    if (this.type == FileType.IMAGE) {
+        throw new WrongFileTypeException();
+    }
+
+    String str = new String(content);
+
+    if (!str.contains("=") || str.startsWith("=") || str.endsWith("=")) {
+        throw new InvalidContentException();
+    }
+
+    for (char c : content) {
+        this.content.add(c);
+    }
+}
     
     /*
      * Method to code/test
      */
-    public void addImageBytes(char[] content) throws InvalidContentException, WrongFileTypeException, WrongEncodingException {
-    	
+    public void addImageBytes(char[] content) 
+        throws InvalidContentException, WrongFileTypeException, WrongEncodingException {
 
-    }			
+    if (content == null) {
+        throw new InvalidContentException();
+    }
+
+    if (this.type == FileType.PROPERTY) {
+        throw new WrongFileTypeException();
+    }
+
+    for (char c : content) {
+        if (c > 255) {
+            throw new WrongEncodingException();
+        }
+        this.content.add(c);
+    }
+}		
     
     /*
      * Method to code/test
      */
     public void removeContent(int numberChars) {
-    	
 
+    if (numberChars >= this.content.size()) {
+        this.content.clear();
+        return;
     }
+
+    for (int i = 0; i < numberChars; i++) {
+        this.content.remove(this.content.size() - 1);
+    }
+}
     
     /*
      * getters
--- src/test/java/es/upm/grise/file/FileTest.java
+++ src/test/java/es/upm/grise/file/FileTest.java
@@ -8,3 +8,45 @@ class CruiseControlTest {
 	public void smokeTest() {}
 	
 }
+@Test
+public void testAddPropertyCorrect() throws Exception {
+    File file = new File(FileType.PROPERTY, new Location("test"));
+    file.addProperty("name=Maria".toCharArray());
+    assertTrue(file.getContent().size() > 0);
+}
+
+@Test(expected = InvalidContentException.class)
+public void testAddPropertyNull() throws Exception {
+    File file = new File(FileType.PROPERTY, new Location("test"));
+    file.addProperty(null);
+}
+
+@Test(expected = WrongFileTypeException.class)
+public void testAddPropertyWrongType() throws Exception {
+    File file = new File(FileType.IMAGE, new Location("test"));
+    file.addProperty("key=value".toCharArray());
+}
+
+@Test
+public void testAddImageBytesCorrect() throws Exception {
+    File file = new File(FileType.IMAGE, new Location("test"));
+    file.addImageBytes(new char[]{10, 20, 30});
+    assertEquals(3, file.getContent().size());
+}
+
+@Test(expected = WrongEncodingException.class)
+public void testAddImageBytesWrongEncoding() throws Exception {
+    File file = new File(FileType.IMAGE, new Location("test"));
+    file.addImageBytes(new char[]{300});
+}
+
+@Test
+public void testRemoveContent() {
+    File file = new File(FileType.PROPERTY, new Location("test"));
+    file.getContent().add('a');
+    file.getContent().add('b');
+
+    file.removeContent(1);
+
+    assertEquals(1, file.getContent().size());
+}
